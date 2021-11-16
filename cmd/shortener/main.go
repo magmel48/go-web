11caba7332c898cdb7b0c4b0009bda2809b11ebc
magmel48 <magmel.programmer@gmail.com>
2021-11-16 14:04:04 +0300

    more methods

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,27 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+)
+
+// PostHandler is receiving a link to make it shorter.
+func PostHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "", http.StatusBadRequest)
+		return
+	}
+
+	_, err := w.Write([]byte("<h1>Hello, World</h1>"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", PostHandler)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
+}
