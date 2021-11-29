2c5ece6d5dbbbd38660b8cbda3202eb9f049ba77
magmel48 <magmel.programmer@gmail.com>
2021-11-29 20:37:06 +0300

    incr-6

--- internal/shortener/shortener.go
+++ internal/shortener/shortener.go
@@ -1,23 +1,44 @@
 package shortener
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
 	"net/url"
+	"os"
+	"strings"
 )
+
+var linksDelimiter = "|"
 
 // Shortener makes links shorter.
 type Shortener struct {
 	prefix string
 	links  map[string]string
+	file   *os.File
 }
 
 // NewShortener creates new shortener.
 func NewShortener(prefix string) Shortener {
-	return Shortener{
+	storagePath := os.Getenv("FILE_STORAGE_PATH")
+	if storagePath == "" {
+		storagePath = "links.txt"
+	}
+
+	file, err := os.OpenFile(storagePath, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0777)
+	if err != nil {
+		panic(err)
+	}
+
+	shortener := Shortener{
 		prefix: prefix,
 		links:  make(map[string]string),
+		file:   file,
 	}
+
+	shortener.retrieveStoredLinks(file)
+
+	return shortener
 }
 
 // MakeShorter makes a link shorter.
@@ -34,6 +55,8 @@ func (s Shortener) MakeShorter(link string) (string, error) {
 	} else {
 		id = fmt.Sprintf("%d", len(s.links)+1)
 		s.links[link] = id
+
+		s.storeLinkOnDisk(link, id)
 	}
 
 	return fmt.Sprintf("%s/%s", s.prefix, id), nil
@@ -48,4 +71,26 @@ func (s Shortener) RestoreLong(id string) (string, error) {
 	}
 
 	return "", errors.New("not found")
+}
+
+func (s Shortener) storeLinkOnDisk(link string, id string) {
+	newRecord := fmt.Sprintf("%s%s%s\n", link, linksDelimiter, id)
+
+	_, err := s.file.Write([]byte(newRecord))
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (s Shortener) retrieveStoredLinks(file *os.File) {
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		line := strings.Split(scanner.Text(), linksDelimiter)
+
+		if len(line) > 1 {
+			s.links[line[0]] = line[1]
+		}
+	}
 }
